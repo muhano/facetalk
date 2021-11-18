242cab0a1b16cbbcfbaa8f95d7d448120c347d9e
Melisa <zheriztin@gmail.com>
2021-11-18 23:46:03 +0700

    refactor

--- controllers/post.js
+++ controllers/post.js
@@ -1,90 +1,90 @@
-const {Post, Profile, Tag, User} = require('../models/index')
+const { Post, Profile, Tag, User } = require('../models/index')
 
-class Controller{
-    static addPostForm(req, res){
-        Tag.findAll()
-            .then(data=> {
-                res.render('addPostForm', {data})
-            })
-            .catch(err=> res.send(err))
-        
-    }
-    static addPost(req, res){
-        const {userId} = req.session
-        let {title, content, imgUrl, TagId} = req.body
-        let newPost = {title, content, imgUrl, TagId, UserId: userId}
-        console.log(newPost);
-        Post.create(newPost)
-            .then(data=> {
-                res.redirect('/home')
-            })
-            .catch(err=> res.send(err))
-    }
+class Controller {
+	static addPostForm(req, res) {
+		Tag.findAll()
+			.then(data => {
+				res.render('addPostForm', { data })
+			})
+			.catch(err => res.send(err))
 
-    static editPostForm(req, res){
-        let tagList;
-        Tag.findAll()
-        .then(data=> {
-            tagList= data
-            return Post.findByPk(req.params.postId)
-        })
-        .then(data=> {
-            res.render('editPostForm', {data, tagList})
-        })
-        .catch(err=> {
-            res.send(err)
-        })
-    }
+	}
+	static addPost(req, res) {
+		const { userId } = req.session
+		let { title, content, imgUrl, TagId } = req.body
+		let newPost = { title, content, imgUrl, TagId, UserId: userId }
+		console.log(newPost);
+		Post.create(newPost)
+			.then(data => {
+				res.redirect('/home')
+			})
+			.catch(err => res.send(err))
+	}
 
-    static editPost(req, res){
-        // console.log(req.params.postId);
-        // console.log(req.body);
-        let {title, content, imgUrl, TagId} = req.body
-        let postContent = {id:req.params.postId, title, content, imgUrl, TagId}
-        // console.log(postContent);
-        Post.upsert(postContent)
-            .then(data=> {
-                res.redirect('/home')
-            })
-            .catch(err=> res.send (err))
-    }
+	static editPostForm(req, res) {
+		let tagList;
+		Tag.findAll()
+			.then(data => {
+				tagList = data
+				return Post.findByPk(req.params.postId)
+			})
+			.then(data => {
+				res.render('editPostForm', { data, tagList })
+			})
+			.catch(err => {
+				res.send(err)
+			})
+	}
 
-    static deletePost(req,res){
-        // console.log(req.params.postId);
-        Post.destroy({
-            where: {
-                id : req.params.postId
-            }
-        })
-        .then(data=> {
-            res.redirect('/home')
-        })
-        .catch(err=> res.send(err))
-        
-    }
+	static editPost(req, res) {
+		// console.log(req.params.postId);
+		// console.log(req.body);
+		let { title, content, imgUrl, TagId } = req.body
+		let postContent = { id: req.params.postId, title, content, imgUrl, TagId }
+		// console.log(postContent);
+		Post.upsert(postContent)
+			.then(data => {
+				res.redirect('/home')
+			})
+			.catch(err => res.send(err))
+	}
 
-    static editProfileForm(req, res) {
-        Profile.findByPk(req.params.profileId)
-            .then(data=> {
-                // console.log(data);
-                res.render('editProfileForm', {data})
-            })
-            .catch(err=> res.send(err))
-    }
+	static deletePost(req, res) {
+		// console.log(req.params.postId);
+		Post.destroy({
+			where: {
+				id: req.params.postId
+			}
+		})
+			.then(data => {
+				res.redirect('/home')
+			})
+			.catch(err => res.send(err))
 
-    static editProfile(req, res) {
-        // console.log(req.params.profileId);
-        // console.log(req.body);
-        Profile.update(req.body, {
-            where: {
-                id: req.params.profileId
-            }
-        })
-        .then(data=> {
-            res.redirect('/home')
-        })
-        .catch(err=> res.send(err))
-    }
+	}
+
+	static editProfileForm(req, res) {
+		Profile.findByPk(req.params.profileId)
+			.then(data => {
+				// console.log(data);
+				res.render('editProfileForm', { data })
+			})
+			.catch(err => res.send(err))
+	}
+
+	static editProfile(req, res) {
+		// console.log(req.params.profileId);
+		// console.log(req.body);
+		Profile.update(req.body, {
+			where: {
+				id: req.params.profileId
+			}
+		})
+			.then(data => {
+				res.redirect('/home')
+			})
+			.catch(err => res.send(err))
+	}
 }
 
 module.exports = Controller
