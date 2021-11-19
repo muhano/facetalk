83277b20ac813887ab125d101d9f4c91d37e8a41
muhano <geri.muhano@gmail.com>
2021-11-19 09:02:16 +0700

    deploy3

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 const Controller = require('./controllers/post')
 const router = require ("./routes/index")
 const session = require('express-session')
